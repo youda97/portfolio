35b50350ae95a54887a6fde4b64e74241e5d3727
youda97 <ouda.yousef@gmail.com>
2025-01-23 17:56:46 -0500

    Add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./components/ThemeContext.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
         <App />
+        <Analytics />
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
